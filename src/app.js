cb930130b0891462e735b5499420d469528d5655
RIO <rio3@pearco.jp>
2019-05-31 16:42:28 +0900

    watcher

--- src/app.js
+++ src/app.js
@@ -1,4 +1,6 @@
 import Vue from 'vue'
+import axios from 'axios'
+import lodash from 'lodash'
 
 var app = new Vue({
   el: '#app',
@@ -87,6 +89,80 @@ var vm = new Vue({
   methods: {
     reverseMessage: function () {
       return this.message.split('').reverse().join('')
+    }
+  }
+})
+
+// 監視プロパティ
+var vm = new Vue({
+  el: '#demo1',
+  data: {
+    firstName: 'Foo',
+    lastName: 'Bar',
+    fullName: 'Foo Bar'
+  },
+  watch: {
+    firstName: function (val) {
+      this.fullName = val + ' ' + this.lastName
+    },
+    lastName: function (val) {
+      this.fullName = this.firstName + ' ' + val
+    }
+  }
+})
+// 算出プロパティ
+var vm = new Vue({
+  el: '#demo2',
+  data: {
+    firstName: 'Foo',
+    lastName: 'Bar'
+  },
+  computed: {
+    fullName: {
+      //getter関数
+      get: function () {
+        return this.firstName + ' ' + this.lastName
+      },
+      // setter 関数
+      set: function (newValue) {
+        var names = newValue.split(' ')
+        this.firstName = names[0]
+        this.lastName = names[names.length - 1]
+      }
+    }
+  }
+})
+
+var watchExampleVM = new Vue({
+  el: '#watch-example',
+  data: {
+    question: '',
+    answer: 'I cannot give you an answer until you ask a question!'
+  },
+  watch: {
+    question: function (newQuestion, oldQuestion) {
+      this.answer = 'Waiting for you to stop typing...'
+      this.debouncedGetAnswer()
+    }
+  },
+  created: function () {
+    this.debouncedGetAnswer = _.debounce(this.getAnswer, 500)
+  },
+  methods: {
+    getAnswer: function () {
+      if (this.question.indexOf('?') === -1) {
+        this.answer = 'Questions usually contain a question mark. ;-)'
+        return
+      }
+      this.answer = 'Thinking...'
+      var vm = this
+      axios.get('https://yesno.wtf/api')
+        .then(function (response) {
+          vm.answer = _.capitalize(response.data.answer)
+        })
+        .catch(function (error) {
+          vm.answer = 'Error! Could not reach the API. ' + error
+        })
     }
   }
 })
