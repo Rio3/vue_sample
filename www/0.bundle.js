9f70d5af7ec9d886c44d04738de3f28eabb18367
RIO <rio3@pearco.jp>
2019-06-18 14:21:21 +0900

    components_dynamic_async

---
+++ www/0.bundle.js
@@ -0,0 +1,15 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([[0],{
+
+/***/ "./src/components/my-async-component.js":
+/*!**********************************************!*\
+  !*** ./src/components/my-async-component.js ***!
+  \**********************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony default export */ __webpack_exports__[\"default\"] = ({\n  template: '<div>my async component</div>'\n});\n\n//# sourceURL=webpack:///./src/components/my-async-component.js?");
+
+/***/ })
+
+}]);
